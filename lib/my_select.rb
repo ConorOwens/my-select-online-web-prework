6f2c1fee7d4f1cc39841b4ee8b50b6ab373d0ec1
Conor Owens <conorj.owens@gmail.com>
2019-03-27 23:52:02 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,11 @@
 def my_select(collection)
  i = 0 
- 
+ selected = []
  while i < collection.length 
    collection.collect do |x|
-     x if x == yield(i)
+     if x == yield(i)
+       selected << x
+     end
    end
    i += 1 
  end
